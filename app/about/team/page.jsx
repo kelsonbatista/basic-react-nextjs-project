2b19beebec4a8a042b063676d39cdc54a255471c
kelsonbatista <kelsonbatista@gmail.com>
2023-04-16 16:18:12 -0300

    feat: added about team page

---
+++ app/about/team/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const metadata = {
+  title: 'About Team Traversy Media',
+  description: 'About Team Traversy Media',
+}
+
+const Team = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum rerum, laboriosam reprehenderit possimus magnam dolore, inventore necessitatibus fugiat facere eos maxime at! Cum quam quibusdam provident minus dolore vitae? Maxime?</p>
+    </div>
+  )
+}
+
+export default Team
