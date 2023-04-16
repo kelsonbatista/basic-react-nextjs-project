361f6ae394df7b48b28daeaf8b3c94d102deac35
kelsonbatista <kelsonbatista@gmail.com>
2023-04-16 17:26:00 -0300

    feat: added header with nav links

---
+++ app/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header>
+      <div className='header'>
+        <div className='container'>
+          <div className='logo'>
+            <Link href='/'>Home</Link>
+          </div>
+          <div className="links">
+            <Link href='/about'>About</Link>
+            <Link href='/about/team'>Our Team</Link>
+            <Link href='/code/repos'>Code</Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
