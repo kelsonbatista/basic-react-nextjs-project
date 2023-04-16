9583a4bd268537844e54f74f131efde94d648bd5
kelsonbatista <kelsonbatista@gmail.com>
2023-04-16 16:17:11 -0300

    feat: added about main layout

---
+++ app/about/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutLayout = ({ children }) => {
+  return (
+    <div>
+      <h2>This is about Layout</h2>
+      { children }
+    </div>
+  )
+}
+
+export default AboutLayout
