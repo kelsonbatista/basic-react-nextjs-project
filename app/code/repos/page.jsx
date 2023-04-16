31413eb61ada2c964428143e5fed4a3a237fb590
kelsonbatista <kelsonbatista@gmail.com>
2023-04-16 19:05:10 -0300

    feat: added repositories page

---
+++ app/code/repos/page.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Link from 'next/link';
+import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa';
+
+const fetchRepos = async () => {
+  const response = await fetch('https://api.github.com/users/kelsonbatista/repos');
+
+  await new Promise(resolve => setTimeout(resolve, 1000)); // wait 1 second
+
+  const repos = await response.json();
+  return repos;
+}
+
+const Repos = async () => {
+  const repos = await fetchRepos();
+
+  return (
+    <div className='repos-container'>
+      <h2>Kelson Repositories</h2>
+      <ul className='repo-list'>
+        {repos.map(repo => (
+          <li key={ repo.id }>
+            <Link href={`/code/repos/${ repo.name }`}>
+              <h3>{ repo.name }</h3>
+              <p>{ repo.description }</p>
+              <div className="repo-details">
+                <span>
+                  <FaStar /> { repo.stargazers_count }
+                </span>
+                <span>
+                  <FaCodeBranch /> { repo.forks_count }
+                </span>
+                <span>
+                  <FaEye /> { repo.watchers_count }
+                </span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Repos
