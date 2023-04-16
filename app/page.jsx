d25b19920693682099dc7a1bca141b6b3d1d7845
kelsonbatista <kelsonbatista@gmail.com>
2023-04-16 16:17:41 -0300

    feat: added main page

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Traversy Media</h1>
+      <ul>
+        <li><Link href='/'>Home</Link></li>
+        <li><Link href='about'>About</Link></li>
+        <li><Link href='about/team'>Team</Link></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Home
