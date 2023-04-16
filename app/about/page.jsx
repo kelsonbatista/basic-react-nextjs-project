02a374dd7c9758e02a263a67f689573472f85868
kelsonbatista <kelsonbatista@gmail.com>
2023-04-16 16:17:53 -0300

    feat: added about page

---
+++ app/about/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const metadata = {
+  title: 'About Traversy Media',
+  description: 'About Traversy Media',
+}
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat esse quia, doloribus ratione ea eum optio facere voluptates assumenda sint eos id, sapiente cupiditate eligendi porro omnis velit ullam ipsum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem possimus, ex, illo fuga quam libero delectus, perspiciatis quasi ratione sunt recusandae totam iusto eveniet veniam molestiae quaerat. Exercitationem, ipsum. Cupiditate.</p>
+    </div>
+  )
+}
+
+export default About
